27d2d0dca52176b91fe5beeb2d26b0dc1b7586de
Michael Rhodes <mirhode@siue.edu>
2018-04-09 21:50:55 -0500

    made .gitignore ignore the workspace

--- src/champ_select/champion.py
+++ src/champ_select/champion.py
@@ -1,8 +1,34 @@
+import json
 
+"""
+A champion in our domain model. It contains all properties that we use to
+compute the proper champion for a user.
+"""
 class Champion(object):
-    def __init__(self):
-        self.attribute1 = None
+    name = None
+    difficulty = None
+    lanes = None
+    tags = None
+    attack_range = None
+
+    def __init__(self, name):
+        self.name = name
 
     @staticmethod
-    def fromJson (jsonFilename):
-        return Champion()
+    def from_json(json_filename):
+        champions = []
+        champions_from_json = {}
+        with open(json_filename, "r") as champion_json:
+            champions_from_json = json.load(champion_json)
+        champions_json_data = champions_from_json["data"]
+
+        for _, champ_data in champions_json_data.items():
+            champ = Champion(champ_data["name"])
+            champ.difficulty = champ_data["info"]["difficulty"]
+            champ.tags = champ_data["tags"]
+            champ.attack_range = champ_data["stats"]["attackrange"]
+            champ.lanes = champ_data["lane"]
+            champions.append(champ)
+
+
+        return champions
