09fc66c62bf072b9a951eda09b87f54802a776cd
Joshua Raben <joshraben@gmail.com>
2018-04-22 18:42:14 -0500

    changes

--- src/champ_select/main.py
+++ src/champ_select/main.py
@@ -35,7 +35,12 @@ def main():
 
 def apply_questions(champions, questions):
     for question in questions:
-        champions = list(question(champions))
+        try:
+            champions = list(question(champions))
+        except TypeError:
+            print(champions)
+            print(question)
+            print(question(champions))
 
         if len(champions) <= 1:
             break
@@ -172,10 +177,9 @@ def objective_based(champions):
     choice = prompt_user("Are you more objective oriented or do you concentrate on your own lane?",
                          ["Teamwork", "Solo"])
     probabilities = {}
+    
     if choice == "Teamwork":
         probabilities = {
-            "SPLITPUSH": -0.65,
-            "MOBILE": -0.35,
             "ENGAGE": 0.75,
             "PEEL": 0.85,
             "POKE": 0.0,
@@ -187,8 +191,6 @@ def objective_based(champions):
         probabilities = {
             "SPLITPUSH": 0.45,
             "MOBILE": 0.55,
-            "ENGAGE": -0.75,
-            "PEEL": -0.85,
             "POKE": 0.0,
             "WAVECLEAR": 0.0,
             "SIEGE": 0.45,
@@ -196,7 +198,14 @@ def objective_based(champions):
         }
 
         for champion in champions:
-            champion.certainty_combined(probabilities[champion.playstyle])
+            count = 0
+            for playstyle in champion.playstyle:
+                if playstyle in probabilities:
+                    champion.certainty_combined(probabilities[playstyle])
+                    count += 1
+
+            if count == 0:
+                champion.certainty_combined(-0.65)
 
         return champions
 
@@ -205,12 +214,12 @@ def split_or_tf(champions):
     choice = prompt_user("Do you like to split push or team fight",
                          ["Split Push", "Team Fight"])
     probabilities = {}
+
     if choice == "Split Push":
         probabilities = {
             "SPLITPUSH": 0.85,
             "MOBILE": 0.35,
             "ENGAGE": 0.15,
-            "PEEL": -0.25,
             "POKE": 0.0,
             "WAVECLEAR": 0.0,
             "SIEGE": 0.25,
@@ -218,7 +227,6 @@ def split_or_tf(champions):
         }
     else:
         probabilities = {
-            "SPLITPUSH": -0.75,
             "MOBILE": 0.65,
             "ENGAGE": 0.25,
             "PEEL": 0.60,
@@ -229,7 +237,14 @@ def split_or_tf(champions):
         }
 
         for champion in champions:
-            champion.certainty_combined(probabilities[champion.playstyle])
+            count = 0
+            for playstyle in champion.playstyle:
+                if playstyle in probabilities:
+                    champion.certainty_combined(probabilities[playstyle])
+                    count += 1
+
+            if count == 0:
+                champion.certainty_combined(-0.65)
 
         return champions
 
@@ -238,31 +253,33 @@ def roaming(champions):
     choice = prompt_user("Do you enjoy roaming to support your team?",
                          ["Yes", "No"])
     probabilities = {}
+
     if choice == "Yes":
         probabilities = {
-            "SPLITPUSH": -0.15,
             "MOBILE": 0.75,
             "ENGAGE": 0.65,
-            "PEEL": -0.25,
-            "POKE": -0.25,
             "WAVECLEAR": 0.15,
             "SIEGE": 0.45,
             "BURST": 0.25
         }
+
     else:
         probabilities = {
             "SPLITPUSH": 0.5,
-            "MOBILE": -0.25,
-            "ENGAGE": -0.15,
             "PEEL": 0.45,
             "POKE": 0.25,
-            "WAVECLEAR": 0.45,
-            "SIEGE": -0.35,
-            "BURST": -0.35
+            "WAVECLEAR": 0.45
         }
 
         for champion in champions:
-            champion.certainty_combined(probabilities[champion.playstyle])
+            count = 0
+            for playstyle in champion.playstyle:
+                if playstyle in probabilities:
+                    champion.certainty_combined(probabilities[playstyle])
+                    count += 1
+
+            if count == 0:
+                champion.certainty_combined(-0.65)
 
         return champions
 
